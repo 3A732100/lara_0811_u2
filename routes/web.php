bddf0410a158d8b850d68ed5c6a56ed98becdfc1
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-10-18 16:32:13 +0800

    練習1.(3)：web.php 變更

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//原route
+//Route::get('/',function(){returnview('welcome');});
 
+//練習1. (1)
+//Route::get('/',function(){return'welcome';});
 
-Route::get('/',function(){
-    return'welcome';
-});
+Route::get('r1', function(){ return redirect('r2'); });
+Route::get('r2', function(){ return view('welcome'); });
